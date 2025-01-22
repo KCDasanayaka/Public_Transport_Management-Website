7f36da7dd19ba706e846e4c6147ffa3e95b2860a
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2025-01-22 23:58:54 +0530

    updated file content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,14 +18,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button  onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p className='text-blue-800 font-mono'>
+        <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs text-red-600">
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
